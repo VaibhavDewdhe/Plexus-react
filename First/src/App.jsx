82004a28861fe9ed284809bf40c6604afad59ed4
Vaibhav Dewdhe <vdewdhe@gmail.com>
2025-12-03 19:23:01 +0530

    first commit

---
+++ First/src/App.jsx
@@ -0,0 +1,12 @@
+import FirstComponent from "./components/FirstComponent"
+import {Siddhi, Rutuja} from './components/Siddhi'
+const App = () => {
+  return <div>
+    <FirstComponent/>
+    <Siddhi/>
+    <Rutuja/>
+  </div>
+}
+
+export default App
+
---
+++ First/src/components/FirstComponent.jsx
@@ -0,0 +1,5 @@
+function FirstComponent(){
+    return <h3>I am h3</h3>
+}
+
+export default FirstComponent;
